793b379ae7317ae76b29fc0c5a878628204585b7
Ulysse Moreau <ulysse.moreaiu@gmx.fr>
2025-04-29 20:03:50 +0200

    Add no-warranty clause

--- tinyutc.h
+++ tinyutc.h
@@ -4,6 +4,14 @@
  * @author Ulysse Moreau
  * @date 2025-04-28
  * @version 1.1
+ * @license WTFPL (Do What The F*ck You Want To Public License)
+ * 
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ *
  */
 
 #ifndef _TINYUTC_H
